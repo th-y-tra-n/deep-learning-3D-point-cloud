c4d7b82e2997c37135cb0143fe8f26fbccfe773d
Thy Tran <augusttran@outlook.com>
2024-05-20 23:28:52 -0400

    Create voxelgrid.py

---
+++ voxel-grids/downsampling/voxelgrid.py
@@ -0,0 +1,15 @@
+# Create a VoxelGrid filter object for our input point cloud
+vox = cloud.make_voxel_grid_filter()
+
+# Choose a voxel (also known as leaf) size
+# Note: this (1) is a poor choice of leaf size   
+# Experiment and find the appropriate size!
+LEAF_SIZE = 1   
+
+# Set the voxel (or leaf) size  
+vox.set_leaf_size(LEAF_SIZE, LEAF_SIZE, LEAF_SIZE)
+
+# Call the filter function to obtain the resultant downsampled point cloud
+cloud_filtered = vox.filter()
+filename = 'voxel_downsampled.pcd'
+pcl.save(cloud_filtered, filename)
